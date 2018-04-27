62f97ad57754dc6f1addb46ce59071356a15976d
Kyle White <kwdwhite@yahoo.com>
2018-04-27 11:35:05 -0400

    second

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express = require("express")
 const port = 3000
 const app = express()
 
+
 app.use(express.static('public'))
 app.use(express.json())
 
+
 // Fill in your request handlers here
+let updates = [];
+
+
+app.post('/updates', function (req, res, next) {
+    let newestUpdate = [];
+    let clientupdates = req.body.clientupdates;
+    let sequenceNum = req.body.sequenceNum;
+
+    // Adds latest updates to updates array
+    updates = updates.concat(clientupdates);
+    // adds latest updates to newestUpdate array
+    newestUpdate = updates.slice(sequenceNum);
+      
+    // Update sequence number
+    sequenceNum = updates.length;
+
+    res.send({updates: newestUpdate, sequenceNum: sequenceNum});
+})
+
 
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,37 @@
 // Add logic to this script to poll server every second for updated pixels.
+let timeoutID;
+let sequenceNum = 0;
+// Poll server every second for updated pixels.
+function fetchUpdates(){
+    const postRequestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({"clientupdates": clientupdates, "sequenceNum": sequenceNum}),
+    }
+
+    fetch('/updates', postRequestOptions)
+    .then(response => response.json())
+    .then(data => {
+        let newestUpdates = data.updates;
+        let latestSequenceNum = data.sequenceNum;
+        // If there are new updates, replay to sync-up
+        for(let i=0; i < newestUpdates.length; i++){
+            bitmap.setColor(newestUpdates[i][0], newestUpdates[i][1], newestUpdates[i][2],true);
+        }
+    
+        // Reset clientupdates
+        clientupdates = [];
+        // Set sequence number to highest-seen index number
+        sequenceNum = latestSequenceNum;
+        // Poll the server every second for updates
+        timeoutID = setTimeout(fetchUpdates, 1000);
+    })
+    .catch(error => {
+        console.log("An error occured when attempted to fetch:", error);
+        clearTimeout(timeoutID);
+    })
+}
+
+fetchUpdates();
